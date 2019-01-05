a6605c8f4920d7bd45d629bcd80c95505059757c
Emmet Murray <ehmurray8@gmail.com>
2019-01-05 00:45:27 -0600

    Tutorial 2: Up to 'Make Flying Objects Appear Randomly' section.

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,9 +4,16 @@ import Sky from './Sky';
 import Ground from "./Ground";
 import CannonPipe from "./CannonPipe";
 import CannonBase from "./CannonBase";
+import CannonBall from './CannonBall';
+import CurrentScore from "./CurrentScore";
+import FlyingObject from "./FlyingObject";
+import Heart from "./Heart";
+import StartGame from "./StartGame";
+import Title from "./Title";
 
 const Canvas = (props) => {
-    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+    const gameHeight = 1200;
+    const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
     return (
         <svg
             id="aliens-go-home-canvas"
@@ -14,10 +21,33 @@ const Canvas = (props) => {
             onMouseMove={props.trackMouse}
             viewBox={viewBox}
         >
+            <defs>
+                <filter id="shadow" >
+                    <feDropShadow dx="1" dy="1" stdDeviation="2"/>
+                </filter>
+            </defs>
             <Sky />
             <Ground/>
             <CannonPipe rotation={props.angle} />
             <CannonBase />
+
+            { ! props.gameState.started &&
+                <g>
+                    <StartGame onClick={() => props.startGame()}/>
+                    <Title/>
+                </g>
+            }
+
+            { props.gameState.started &&
+                <g>
+                    <CannonBall position={{x: 0, y: -100}}/>
+                    <CurrentScore score={123}/>
+                    <FlyingObject position={{x: -150, y: -300}}/>
+                    <FlyingObject position={{x: 150, y: -300}}/>
+                    <Heart position={{x: -300, y: 35}}/>
+                </g>
+            }
+
         </svg>
     );
 };
